6f709fd506cda96ceb1dd496b2d2b54376cc5ae5
squidleader <zakrevsky_2@yahoo.com>
2024-12-20 15:04:17 -0500

    Add styles.css Change html to use a form for searching Change search to take input for a stock to look for

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, Query, Request
+from fastapi import FastAPI, Request, Query, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from elasticsearch import Elasticsearch
-from typing import Optional, List
-
+from typing import List
 from classes.CommentData import CommentData
 
 # don't forget to change username and password before deployment
@@ -15,59 +15,54 @@ es = Elasticsearch(
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 #to run this, navigate to the root (wsb-elastc-fastapi) in terminal
 # uvicorn main:app --reload
 #returns a raw string that matches the hit of the query
-@app.get("/search/")
-async def search(request: Request, query: str | str = "nvidia"): #this passes the required string or makes the default /search/?query=nvidia
-#async def search(query: Optional[str] = Query(None)): #this defaults to an empty query /search/?query=None
+@app.get("/", response_class=HTMLResponse)
+@app.post("/", response_class=HTMLResponse)
+async def search(request: Request, query: str = Form("nvidia")): #will default to "nvidia" on page load
+    results: List[CommentData] = []
 
-    #this can be used to define the query within the function
-    query = "msft"
-
-    if not query:
-        return {"error": "Query parameter 'query' is required."}
-
-    try:
-        # performs a search on the 'comment' field of wsb_comments
-        # the term is then specified here: http://127.0.0.1:8000/search/?query=TERM
-        # for nvidia == http://127.0.0.1:8000/search/?query=nvidia
-        resp = es.search(index="wsb_comments", body={
-            "query": {
-                "match": {
-                    "comment": query
+    if query:
+        try:
+            # performs a search on the 'comment' field of wsb_comments
+            # the term is then specified here: http://127.0.0.1:8000/search/?query=TERM
+            # for nvidia == http://127.0.0.1:8000/search/?query=nvidia
+            resp = es.search(index="wsb_comments", body={
+                "query": {
+                    "match": {
+                        "comment": query
+                    }
                 }
-            }
-        })
+            })
 
-        hits = resp['hits']['total']['value']
-        raw_results = resp['hits']['hits']
+            raw_results = resp['hits']['hits']
 
-        #convert reults into the CommentData class
-        #create a list containing them
-        #return them to fastAPI in object form
-        results: List[CommentData] = []
-        for result in raw_results:
-            source = result["_source"]
-            comment_data = CommentData(
-                username=source["username"],
-                comment=source["comment"],
-                date=source["date"]
-            )
-            results.append(comment_data)
-        """
-        return {
-            "message": f"Got {hits} hits",
-            "results": [result.to_dict() for result in results]
-        }"""
-        return templates.TemplateResponse("table.html",{
-            "request": request,
-            "results": results
-        })
+            # convert reults into the CommentData class
+            # create a list containing them
+            # return them to fastAPI in object form
+            for result in raw_results:
+                source = result["_source"]
+                results.append(CommentData(
+                    username=source["username"], #split at the "• 16h ago •" in the future to remove it
+                    comment=source["comment"],
+                    date=source["date"]
+                ))
+        except Exception as e:
+            return templates.TemplateResponse("table.html", {
+                "request": request,
+                "results": [],
+                "error": str(e)
+            })
+    #for debugging
+    #print(f"Query: {query}")
+    #print(f"Results: {results}")
 
-    except Exception as e:
-        return {"error": str(e)}
-
-
+    return templates.TemplateResponse("table.html", {
+        "request": request,
+        "results": results,
+        "query": query
+    })
 
